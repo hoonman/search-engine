2012e312c0bae226e1489a1b72e50f2c92cfeec0
hoonman <jh020812@gmail.com>
2025-08-15 15:04:21 -0700

    normalize token, compute word frequencies, print frequencies functions for the tokenizer

--- src/tokenizer/tokenizer.py
+++ src/tokenizer/tokenizer.py
@@ -1,5 +1,6 @@
 import os
 import json
+import re
 
 class Tokenizer:
     def __init__(self, filepath):
@@ -17,31 +18,59 @@ class Tokenizer:
         # steps: 
         # lets use with open(filepath, 'r') to read the HTML / PDF file
         # lets focus on text files first. and come up with cases along the way
-        #`get the raw_content line by line and parse the tokens line by line since doing it all at once is bad since it loads everything at once
+        # get the raw_content line by line and parse the tokens line by line since doing it all at once is bad since it loads everything at once
         # break the line by whitespace and for each of these, filter out all the non-alphanumeric characters.
         # let's see the result after this.
 
+        # choices:
+        # lowercase, alphanumeric, what else should we keep? emails? dots? (for links), 
+        # before normalizing, we should consider if we should filter the emails, links,
+
         with open(self.filepath, 'r') as f:
             for line in f:
-                print("line: ", line)
-                tokens = line.split(' ')
-                print("tokens: ", tokens)
-        pass
+                tokens = line.strip().split() # strips the first and last trailing whitespace and splits on any number of whitespace
+                for i, token in enumerate(tokens):
+                    tokens[i] = self.normalize_token(token)
 
-    def compute_word_frequencies(self, tokens):
+                self.tokens.extend(tokens)
+
+    def normalize_token(self, token):
+        '''
+        this function will take a single token as input and normalize it by:
+        1. filter non-alphanumeric characters
+        2. lowercase the characters
+        3. unicode 
+        '''
+        token = token.casefold() 
+        token = self.filter_alphanumeric(token)
+        return token
+
+    def filter_alphanumeric(self, token):
+        # . / : 
+        # - for dates
+        # @ for emails
+        return re.sub(r'[^a-z0-9\s]', ' ', token)
+
+    # def filter_punctuations(self, token):
+
+    def compute_word_frequencies(self):
         '''
         count the number of occurrences of each token in the tokens input list. 
         '''
-        pass
+        for token in self.tokens:
+            if token in self.frequency_map:
+                self.frequency_map[token] += 1
+            else:
+                self.frequency_map[token] = 1
 
-    def print(self, frequency_map):
+    def print_frequencies(self):
         '''
         this method prints out the word frequency onto the command line interface. 
         print should be ordered by frequency.
         use alphabet ordering for tie breaks
         '''
-        for token, freq in frequency_map.items():
+        sorted_frequency_map = dict(sorted(self.frequency_map.items(), key=lambda x: x[1], reverse=True))
+        for token, freq in sorted_frequency_map.items():
             print(f"{token} - {freq}")
-            print("\n")
 
     
