5d8b50beccc8595320e58069d7ea7c46ebe2ed09
hoonman <jh020812@gmail.com>
2025-08-16 10:29:55 -0700

    modify tokenizer.main using module import

--- src/tokenizer/main.py
+++ src/tokenizer/main.py
@@ -1,12 +1,12 @@
-from cli import ArgParser
-from tokenizer import Tokenizer
+from .cli import ArgParser
+from .tokenizer import Tokenizer
 
 def main():
     parser = ArgParser()
     args = parser.parse()
-    print("received filepath: ", parser.filepath)
-    filepath = parser.filepath
-    tokenizer = Tokenizer(filepath)
+    print("received filepath: ", parser.filepaths)
+    filepaths = parser.filepaths
+    tokenizer = Tokenizer(filepaths[0])
     tokenizer.tokenize()
     tokenizer.compute_word_frequencies()
     tokenizer.print_frequencies()
