3b4352e46483c34ea4988c310b5e13a54a80c885
hoonman <jh020812@gmail.com>
2025-08-30 21:05:19 +0900

    bs4 practice

--- whiteboard.py
+++ whiteboard.py
@@ -1,1 +1,18 @@
 # sample whiteboard file for first commit 
+
+# beautifulsoup testing
+
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://quotes.toscrape.com"
+resp = requests.get(url)
+resp.raise_for_status()
+
+soup = BeautifulSoup(resp.text, "lxml")
+print(soup.title.string)
+
+for q in soup.select(".quote .text"):
+    print(q.get_text(strip=True))
+
+    
