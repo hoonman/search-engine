db9e0a37d0600c6d558fa3d6aa85859f95af43cb
hoonman <jh020812@gmail.com>
2025-09-02 06:35:20 -0700

    encapsulate scraper logic into a class

--- src/web_crawler/scraper.py
+++ src/web_crawler/scraper.py
@@ -2,7 +2,51 @@ import re
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urljoin
 
-unique_links = set()
+class Scraper:
+    def __init__(self):
+        self.visited_links = set()
+    
+    def scraper(self, url, resp):
+        links = self.extract_next_links(url, resp)
+        return [link for link in links if is_valid(link)]
+
+    def extract_next_links(self, url, resp):
+        links = []
+        if resp.status != 200:
+            return []
+
+        if resp.raw_response and resp.raw_response['content']:
+            soup = BeautifulSoup(resp.raw_response['content'], "lxml") # uses lxml parser
+            for link in soup.find_all('a'):
+                parsed_url = link.get('href')
+                joined_url = urljoin(url, parsed_url)
+                print("joined url: ", joined_url)
+                links.append(joined_url)
+        return links
+
+    def is_valid(url):
+    # Decide whether to crawl this url or not. 
+    # If you decide to crawl it, return True; otherwise return False.
+    # There are already some conditions that return False.
+        try:
+            parsed = urlparse(url)
+            if parsed.scheme not in set(["http", "https"]):
+                return False
+            return not re.match(
+                r".*\.(css|js|bmp|gif|jpe?g|ico"
+                + r"|png|tiff?|mid|mp2|mp3|mp4"
+                + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+                + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
+                + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
+                + r"|epub|dll|cnf|tgz|sha1"
+                + r"|thmx|mso|arff|rtf|jar|csv"
+                + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+
+        except TypeError:
+            print ("TypeError for ", parsed)
+            raise
+
+
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
