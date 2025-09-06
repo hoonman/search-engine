5c1e852168bea9bde51577c4ba0b454cf9d66d80
hoonman <jh020812@gmail.com>
2025-09-06 04:53:40 -0700

    remove extra delay during direct download

--- src/web_crawler/utils/direct_downloader.py
+++ src/web_crawler/utils/direct_downloader.py
@@ -5,30 +5,8 @@ import pickle
 from utils.response import Response
 from urllib.parse import urlparse
 
-domain_last_accessed = {}
-global_request_count = 0
-MAX_REQUESTS = 200
-
 def direct_download(url, config):
     '''Downloads content directly from the web without using cache server'''
-    global global_request_count
-    domain = urlparse(url).netloc
-
-    if global_request_count >= MAX_REQUESTS:
-        return Response({
-            "url": url,
-            "status": 429,
-            "error": "Max pages limit reached",
-        })
-    global_request_count += 1
-
-    if domain in domain_last_accessed:
-        elapsed = time.time() - domain_last_accessed[domain]
-        if elapsed < config.time_delay:
-            time.sleep(config.time_delay - elapsed + random.uniform(0, 0.1))
-
-    domain_last_accessed[domain] = time.time()
-
     try:
         headers = {
             'User-Agent': config.user_agent
