8da53aa10a151815a40914e645a512a9667d6d0e
hoonman <jh020812@gmail.com>
2025-10-13 12:58:50 -0700

    corresponding changes in scraper and worker for robots modularization

--- src/web_crawler/crawler/worker.py
+++ src/web_crawler/crawler/worker.py
@@ -44,4 +44,4 @@ class Worker(Thread):
                 self.frontier.add_url(scraped_url)
             self.frontier.mark_url_complete(tbd_url)
             self.page_count += 1
-            time.sleep(self.scraper.get_robots_delay(tbd_url))
+            time.sleep(self.scraper.robots_parser.get_robots_delay(tbd_url))
--- src/web_crawler/scraper.py
+++ src/web_crawler/scraper.py
@@ -3,7 +3,7 @@ import json
 import time
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urljoin, urlsplit, urlunsplit
-from urllib.robotparser import RobotFileParser
+from robots import RobotsParser
 from utils import get_logger
 
 class Scraper:
@@ -14,10 +14,8 @@ class Scraper:
         self.config = config # we can use this for similarity threshold later.
         self.start_time = time.time()
         self.subdomains = {}
-        self.robot_parser = RobotFileParser()
-        self.robots_cache = {}
-        self.robots_delay = {}
         self.agent_name = "ICSCrawler"
+        self.robots_parser = RobotsParser(self.agent_name, config)
 
         # similarity url tracking
 
@@ -62,10 +60,7 @@ class Scraper:
             if parsed.scheme not in set(["http", "https"]):
                 return False
                 
-            robots_parser = self.get_robots_parser(url, parsed)
-            self.check_robots_delay(url, parsed)
-            if robots_parser and not robots_parser.can_fetch(self.agent_name, url):
-                self.logger.debug(f"Robots.txt disallows crawling: {url}")
+            if not self.robots_parser.check_robots(url, parsed):
                 return False
                 
             if not self.filter_valid_domains(url, parsed):
@@ -122,49 +117,6 @@ class Scraper:
                 self.subdomains[scheme_hostname] += 1
             else:
                 self.subdomains[scheme_hostname] = 1
-
-    def get_robots_parser(self, url, parsed):
-        '''
-        takes url that we are validating, parsed object from urlparse and returns a robots parser using RobotFileParser
-        '''
-        try:
-            robots_url = f"{parsed.scheme}://{parsed.netloc}/robots.txt"
-            if robots_url in self.robots_cache:
-                return self.robots_cache[robots_url]
-
-            rp = RobotFileParser()
-            rp.set_url(robots_url)
-            return self.load_robots_parser(rp, robots_url)
-        except Exception as e:
-            self.logger.error(f"Error creating robots parser for {url}: {e}")
-            return None
-
-    def load_robots_parser(self, rp, robots_url):
-        try:
-            rp.read()
-            self.robots_cache[robots_url] = rp
-            return rp
-        except Exception as e:
-            self.logger.warning(f"could not read robots.txt from {robots_url}: {e}")
-            fallback_rp = RobotFileParser()
-            fallback_rp.set_url(robots_url)
-            self.robots_cache[robots_url] = fallback_rp
-            return fallback_rp
-
-    def check_robots_delay(self, url, parsed):
-        rp = self.get_robots_parser(url, parsed)
-        if rp:
-            delay = rp.crawl_delay(self.agent_name)
-            if delay:
-                self.robots_delay[url] = delay
-
-    def get_robots_delay(self, url):
-        if url in self.robots_delay:
-            return self.robots_delay[url]
-        return self.config.time_delay
-
-    def normalize_url(self):
-        pass
 
     def append_to_json_file(self, filename, data):
         try:
