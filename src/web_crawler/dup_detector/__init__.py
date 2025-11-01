90a64a00999a1a56afc6146c962e970ace8530ba
hoonman <jh020812@gmail.com>
2025-11-01 07:13:35 -0700

    checksum sha 256

--- src/web_crawler/dup_detector/__init__.py
+++ src/web_crawler/dup_detector/__init__.py
@@ -14,4 +14,19 @@ class ExactDupDetector:
     def __init__(self):
         self.seen_checksum = set()
 
-    
+    def compute_checksum_sha256(self, content):
+        if isinstance(content, str):
+            content = content.encode('utf-8')
+        return hashlib.sha256(content).hexdigest()
+
+    def is_exact_duplicate(self, content):
+        curr_checksum = self.compute_checksum_sha256(content)
+        if curr_checksum in self.seen_checksum:
+            return True
+        self.seen_checksum.add(curr_checksum)
+        return False
+
+
+class SimDupDector:
+    def __init__(self):
+        pass
